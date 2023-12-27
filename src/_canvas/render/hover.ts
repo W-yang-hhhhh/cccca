b88f796526a29eb8a6b33731c07334d9d3041663
wangyang <yang.wang@js.design>
2023-12-27 11:31:04 +0800

    fix: fix

--- src/_canvas/render/hover.ts
+++ src/_canvas/render/hover.ts
@@ -1,10 +1,9 @@
 // render select
-
-import Text from "../shapes/text";
+import { AElementType } from "../types/element";
 
 const POINT_SIZE = 8;
 const POINT_SIZE_OFFSET = POINT_SIZE / 2;
-export const renderHover = (ctx: CanvasRenderingContext2D, data: Text) => {
+export const renderHover = (ctx: CanvasRenderingContext2D, data: AElementType) => {
   const _data = data.getElementData();
   const { width, height, x, y, angle } = _data;
   ctx.save();
@@ -12,9 +11,9 @@ export const renderHover = (ctx: CanvasRenderingContext2D, data: Text) => {
   ctx.strokeStyle = "rgb(29,128,255)";
   ctx.fillStyle = "#fff";
   ctx.lineWidth = 1;
-  ctx.translate(x + width/2,y+height/2);
+  ctx.translate(x + width / 2, y + height / 2);
   ctx.rotate(angle);
-  ctx.rect(-width/2, -height/2, width, height);
+  ctx.rect(-width / 2, -height / 2, width, height);
   ctx.stroke();
   ctx.restore();
 };
