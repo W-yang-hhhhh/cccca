dfc0ebe1b623884476d5fffbe863e3032296958e
wangyang <yang.wang@js.design>
2024-01-03 11:49:38 +0800

    fix: fix hover style

--- src/_canvas/render/hover.ts
+++ src/_canvas/render/hover.ts
@@ -1,11 +1,13 @@
 // render select
+import { getTextWidth } from "../helper/text";
 import { AElementType } from "../types/element";
 
 const POINT_SIZE = 8;
 const POINT_SIZE_OFFSET = POINT_SIZE / 2;
 export const renderHover = (ctx: CanvasRenderingContext2D, data: AElementType) => {
   const _data = data.getElementData();
-  const { width, height, x, y, angle } = _data;
+  const { width, height, x, y, angle,text,fontSize,fontFamily } = _data;
+  const textSplit = text.split('\n');
   ctx.save();
   ctx.beginPath();
   // ctx.strokeStyle = "rgb(29,128,255)";
@@ -15,8 +17,12 @@ export const renderHover = (ctx: CanvasRenderingContext2D, data: AElementType) =
   ctx.translate(x + width / 2, y + height / 2);
   ctx.rotate(angle);
   // ctx.rect(-width / 2, -height / 2, width, height);
-  ctx.moveTo(-width / 2, -height / 2 + height)
-  ctx.lineTo(-width / 2 + width, -height / 2 + height);
-  ctx.stroke();
+  textSplit.map((item,index)=>{
+    const textWidth = getTextWidth(item,fontSize,fontFamily)
+    ctx.moveTo(-width / 2, -height / 2 + fontSize*(index + 1));
+    ctx.lineTo(-width / 2 + textWidth, -height / 2 + fontSize*(index + 1));
+    ctx.stroke();
+  })
+  
   ctx.restore();
 };
