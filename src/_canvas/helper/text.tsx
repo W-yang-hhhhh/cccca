15b7997337c33c681bf7076d36ecfa440b6edf20
wangyang <yang.wang@js.design>
2024-01-08 10:37:05 +0800

    fix: 缓存数据

--- src/_canvas/helper/text.tsx
+++ src/_canvas/helper/text.tsx
@@ -3,29 +3,56 @@ import { TextAreaComp } from "../components/TextArea";
 import { getElementById } from ".";
 import { AElementType, elementType } from "../types/element";
 //获取文本宽度
+
+let cacheText = "";
+let cacheFontSize = 0;
+let cacheFontFamily = "";
+let cacheMaxLengthText = 0;
 export const getTextWidth = (
   text: string,
   fontSize: number,
   fontFamily: string
 ) => {
+  if (
+    cacheText === text &&
+    cacheFontSize === fontSize &&
+    cacheFontFamily === fontFamily &&
+    cacheMaxLengthText
+  ) {
+    return cacheMaxLengthText;
+  }
+  cacheText = text;
+  cacheFontSize = fontSize;
+  cacheFontFamily = fontFamily;
+
   let OsCanvas = new OffscreenCanvas(100, 100);
   let OsContext = OsCanvas.getContext(
     "2d"
   ) as OffscreenCanvasRenderingContext2D;
   const textArr = text.split("\n");
   OsContext.font = `${fontSize}px ${fontFamily}`;
-  let maxLengthText = textArr.sort(
-    (a: string, b: string) => b.length - a.length
-  )[0];
-  const _w = OsContext.measureText(maxLengthText).width;
-  return _w;
+  let maxLengthText = 0;
+  //   let maxLengthText = textArr.sort(
+  //     (a: string, b: string) => b.length - a.length
+  //   )[0];
+  textArr.forEach((item) => {
+    let width = OsContext.measureText(item).width;
+    if (width > maxLengthText) maxLengthText = width;
+  });
+  //   const _w = OsContext.measureText(maxLengthText).width;
+  cacheMaxLengthText = maxLengthText;
+  return maxLengthText;
 };
 
 export const getFonSizeByHeight = (height: number, colNum: number) => {
   return height / colNum;
 };
 
-export const TextToEditMode = function(this: any, elements: AElementType[], id: string)  {
+export const TextToEditMode = function (
+  this: any,
+  elements: AElementType[],
+  id: string
+) {
   const currentElement = getElementById(elements, id);
   const currentElementData = currentElement?.getElementData();
   if (!currentElement || currentElementData?.elementType !== elementType.text) {
@@ -38,8 +65,8 @@ export const TextToEditMode = function(this: any, elements: AElementType[], id: 
     currentElementData;
 
   const blurEventHandle = getBlurEvent(currentElement);
-  const maxWidth = getMaxTextAreaWidth(this,currentElement); 
-  const maxHeight = getMaxTextAreaHeight(this,currentElement); 
+  const maxWidth = getMaxTextAreaWidth(this, currentElement);
+  const maxHeight = getMaxTextAreaHeight(this, currentElement);
   const props = {
     x: x,
     y: y,
@@ -78,26 +105,25 @@ const getBlurEvent = (currentElement: AElementType) => {
   };
 };
 
+const getMaxTextAreaWidth = (context: any, currentElement: AElementType) => {
+  const { x, width, angle } = currentElement.getElementData();
 
-const getMaxTextAreaWidth = (context:any,currentElement: AElementType)=>{
-    const {x,width,angle} = currentElement.getElementData();
-    
-    const canvasWidth = parseInt(context.ctx.canvas.style.width);
-    if(x<0){
-        return width + x
-    }
-    const cosW = (canvasWidth - x) 
-    console.log('canvasWidth',cosW)
-    return cosW
-}
+  const canvasWidth = parseInt(context.ctx.canvas.style.width);
+  if (x < 0) {
+    return width + x;
+  }
+  const cosW = canvasWidth - x;
+  console.log("canvasWidth", cosW);
+  return cosW;
+};
 
-const getMaxTextAreaHeight = (context:any,currentElement: AElementType)=>{
-    const {y,height,width,angle} = currentElement.getElementData();
-    const canvasHeight = parseInt(context.ctx.canvas.style.height);
-    if(y<0){
-        return height + y
-    }
-    const _y = y - (width/ 2) * Math.cos(angle)
-    const cosH = (canvasHeight - _y) 
-    return cosH
-}
+const getMaxTextAreaHeight = (context: any, currentElement: AElementType) => {
+  const { y, height, width, angle } = currentElement.getElementData();
+  const canvasHeight = parseInt(context.ctx.canvas.style.height);
+  if (y < 0) {
+    return height + y;
+  }
+  const _y = y - (width / 2) * Math.cos(angle);
+  const cosH = canvasHeight - _y;
+  return cosH;
+};
